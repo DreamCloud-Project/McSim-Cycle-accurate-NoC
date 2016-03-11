f54263e0a1ec00c8a31d50b7d5debd649e1fe21c
Manuel Selva <manuel.selva@lirmm.fr>
2016-03-11 10:54:08 +0100

    first version of CA on github

--- compile.py
+++ compile.py
@@ -10,38 +10,33 @@ def main():
 
     # Configure parameters parser
     parser = argparse.ArgumentParser(description='Cycle Accurate Simulator Compiler script')
+    parser.add_argument('-v',  '--verbose', action='store_true', help='enable verbose output')
     subparsers = parser.add_subparsers(title='valid subcommands', dest='command')
     parser_cp = subparsers.add_parser('build')
-    parser_cp.add_argument("-c", '--clean', action='store_true', help='clean previously compiled files before compiling')
-    parser_cp.add_argument('-v',  '--verbose', action='store_true', help='enable verbose output')
     parser_cl = subparsers.add_parser('clean')
-    parser_cl.add_argument('-v',  '--verbose', action='store_true', help='enable verbose output')
     args = parser.parse_args()
 
-    # Run cmake, clean, build
+    # Clean in all cases
+    print ('Cleaning')
     work_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'obj')
+    if os.path.exists(work_dir):
+        shutil.rmtree(work_dir)
 
-    if args.command == 'clean' or args.clean:
-        print ('Cleaning the abstract simulator previous build ...')
-        for root, dirs, files in os.walk(work_dir):
-            for f in files:
-                os.unlink(os.path.join(root, f))
-            for d in dirs:
-                shutil.rmtree(os.path.join(root, d))
-
-    print ('Running cmake')
-    cmd = ['cmake', '..']
-    if not args.verbose:
-        cmake = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=work_dir)
-    else:
-        print (cmd)
-        cmake = subprocess.Popen(cmd, cwd=work_dir)
-    if cmake.wait() != 0:
-        print ('cmake FAILED')
-        exit(-1)
-    print ('cmake done')
-
+    # Build if needed
     if args.command == 'build':
+        os.makedirs(work_dir)
+        print ('Running cmake')
+        cmd = ['cmake', '..']
+        if not args.verbose:
+            cmake = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=work_dir)
+        else:
+            print (cmd)
+            cmake = subprocess.Popen(cmd, cwd=work_dir)
+        if cmake.wait() != 0:
+            print ('cmake FAILED')
+            exit(-1)
+        print ('cmake done')
+        print ('Running make')
         cmd = ['make']
         if not args.verbose:
              build = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=work_dir)
